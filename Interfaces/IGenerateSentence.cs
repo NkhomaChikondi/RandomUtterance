7280550b259796c18256b85a4cd77b311655b2ae
Chikondi Nkhoma <61491585+NkhomaChikondi@users.noreply.github.com>
2023-09-03 13:16:17 +0200

    added in interfaces and services

---
+++ Interfaces/IGenerateSentence.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomUtterance.Interfaces
+{
+    public interface IGenerateSentence
+    {
+        string generateSentence(int adjectiveNumber, bool randomSentence);
+        void getUserInput();
+    }
+}
---
+++ Services/GenerateSentenceService.cs
@@ -0,0 +1,59 @@
+﻿using RandomUtterance.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomUtterance.Services
+{
+    public class GenerateSentenceService : IGenerateSentence
+    {
+        public string generateSentence(int adjectiveNumber, bool randomSentence)
+        {
+            // create a list of adjectives 
+            List<string> adjectives = new List<string>
+            {
+                "Beautiful","Colorful","Exciting","Delicious","Mysterious","Elegant","Vibrant","Serene","Captivating","Enchanting","Mesmerizing","Tranquil"
+            };
+            // Noun (you can replace this with a list of nouns)
+            string noun = "item";
+
+            // Select random adjectives
+            List<string> selectedAdjectives = new List<string>();
+            if (randomSentence)
+            {
+                for (int i = 0; i < adjectiveNumber; i++)
+                {
+                    int index = random.Next(adjectives.Count);
+                    selectedAdjectives.Add(adjectives[index]);
+                }
+            }
+            else
+            {
+                selectedAdjectives = adjectives.Take(adjectiveNumber).ToList();
+            }
+
+            // Generate the sentence
+            string adjectiveString = string.Join(", ", selectedAdjectives);
+            string sentence = $"A {adjectiveString} {noun}";
+
+            return sentence;
+        }
+        public void getUserInput()
+        {
+            int numberofSentence = 0;
+            int numberofAdjectives = 0;
+            Console.WriteLine("Welcome! lets generate interesting sentences");
+            try
+            {
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
